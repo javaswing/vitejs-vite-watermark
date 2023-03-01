e57beebedc3d2ac2433c9b10353494a42925f0e7
javaSwing <zxdlovejava@gmail.com>
2023-03-01 23:55:08 +0800

    feat: add observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css';
-import WaterMark from './components/water-mark';
+import "./App.css";
+import WaterMark from "./components/water-mark";
 
 function App() {
   return (
     <div className="App">
-      <WaterMark content="这是一个水印">
+      <WaterMark content="这是一个水印2">
         <div
-          style={{ width: '300px', height: '600px', background: '#ddd' }}
+          style={{ width: "300px", height: "600px", background: "#ddd" }}
         ></div>
       </WaterMark>
     </div>
--- src/components/water-mark/component.tsx
+++ src/components/water-mark/component.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { WaterMarkProps } from './type';
+import { MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
+import { WaterMarkProps } from "./type";
 
 const WaterMark = (props: WaterMarkProps) => {
   const {
@@ -12,7 +12,7 @@ const WaterMark = (props: WaterMarkProps) => {
     className,
     style = {},
     children,
-    content = '',
+    content = "",
   } = props;
 
   let gapX = x; // 水平间隙
@@ -21,19 +21,17 @@ const WaterMark = (props: WaterMarkProps) => {
   const offsetLeft = gapX / 2;
   const offsetTop = gapY / 2;
 
-  const cavasRef = useRef<HTMLCanvasElement | null>(null);
-  const [base64Url, setBase64Url] = useState('');
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [base64Url, setBase64Url] = useState("");
+
+  const watermarkRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     // 1. 创建canvas 对象
-    if (!cavasRef.current) {
-      cavasRef.current = document.createElement('canvas');
+    if (!canvasRef.current) {
+      canvasRef.current = document.createElement("canvas");
     }
-  }, [cavasRef.current]);
-
-  const ctx = useMemo(() => {
-    return cavasRef.current?.getContext('2d');
-  }, [cavasRef.current]);
+  }, [canvasRef.current]);
 
   // 获取设备像素比
   const ratio = window.devicePixelRatio | 1;
@@ -46,70 +44,99 @@ const WaterMark = (props: WaterMarkProps) => {
   const canvasHeight = (gapY + height) * ratio;
 
   useEffect(() => {
-    if (cavasRef.current) {
-      cavasRef.current.width = width;
-      cavasRef.current.height = height;
-      cavasRef.current.style.background = '#d1eee0';
-      cavasRef.current.setAttribute('width', `${canvasWidth}px`);
-      cavasRef.current.setAttribute('height', `${canvasHeight}px`);
-    }
-  }, [cavasRef.current]);
+    if (canvasRef.current) {
+      canvasRef.current.width = width;
+      canvasRef.current.height = height;
+      canvasRef.current.style.background = "#d1eee0";
+      canvasRef.current.setAttribute("width", `${canvasWidth}px`);
+      canvasRef.current.setAttribute("height", `${canvasHeight}px`);
 
+      const ctx = canvasRef.current.getContext("2d");
+      if (ctx) {
+        /**
+         * 平移当前canvas的原点
+         * offsetLeft offsetRight为间隙的一半
+         * 这段设置的作用是用于调整canvas的画笔位置，让下笔点在对于双边的间隙都是相等（即让水印在canvas居中位置出现）
+         */
+        ctx.translate(offsetLeft * ratio, offsetTop * ratio);
+
+        /**
+         * 旋转canvas，默认顺时针旋转
+         * (Math.PI / 180) * number 把角度转换为弧度，canvas API参数类型为弧度
+         */
+        ctx.rotate((Math.PI / 180) * Number(rotate));
+
+        // 设置画笔的透明度
+        ctx.globalAlpha = alpha;
+
+        // 设置水印的位置区域
+        const markWidth = width * ratio;
+        const markHeight = height * ratio;
+        ctx.fillStyle = "transparent";
+        ctx.fillRect(0, 0, markWidth, markHeight);
+
+        // 样式默认值
+        const fontSize = 16;
+        const fontWeight = "normal";
+        const fontColor = "rgba(0, 0, 0, 0.1)";
+
+        // 画字
+        const markSize = Number(fontSize) * ratio;
+        // 参考：https://developer.mozilla.org/zh-CN/docs/Web/CSS/font
+        // marHeight在这里是行高
+        ctx.font = `normal normal ${fontWeight} ${markSize}px/${markHeight}px undefined`;
+        ctx.textAlign = "start";
+        ctx.textBaseline = "top";
+        ctx.fillStyle = fontColor;
+        ctx.fillText(content, 0, 0);
+
+        canvasRef.current && setBase64Url(canvasRef.current.toDataURL());
+      }
+    }
+  }, [canvasRef.current]);
+
+  // 监听dom修改如果用户修改了dom，再进行动态添加水印,利用 MutationObserver
+  const config = { attributes: true, childList: true, subtree: true };
+  const parent = useRef<HTMLElement>(null);
+
+  // 监听当前节点修改的问题
+  const observer = new MutationObserver(
+    (mutations: MutationRecord[], observer: MutationObserver) => {
+      mutations.forEach((mutation) => {
+        console.log("mutation", mutation);
+        if (mutation.type === "childList") {
+          const removeNodes = mutation.removedNodes;
+          removeNodes.forEach((node) => {
+            const e = node as HTMLElement;
+            // if (e === watermarkRef.current) {
+            watermarkRef.current?.appendChild(e);
+            // }
+          });
+        }
+      });
+    }
+  );
   useEffect(() => {
-    if (ctx) {
-      /**
-       * 平移当前canvas的原点
-       * offsetLeft offsetRight为间隙的一半
-       * 这段设置的作用是用于调整canvas的画笔位置，让下笔点在对于双边的间隙都是相等（即让水印在canvas居中位置出现）
-       */
-      ctx.translate(offsetLeft * ratio, offsetTop * ratio);
-
-      /**
-       * 旋转canvas，默认顺时针旋转
-       * (Math.PI / 180) * number 把角度转换为弧度，canvas API参数类型为弧度
-       */
-      ctx.rotate((Math.PI / 180) * Number(rotate));
-
-      // 设置画笔的透明度
-      ctx.globalAlpha = alpha;
-
-      // 设置水印的位置区域
-      const markWidth = width * ratio;
-      const markHeight = height * ratio;
-      ctx.fillStyle = 'transparent';
-      ctx.fillRect(0, 0, markWidth, markHeight);
-
-      // 样式默认值
-      const fontSize = 16;
-      const fontWeight = 'normal';
-      const fontColor = 'rgba(0, 0, 0, 0.1)';
-
-      // 画字
-      const markSize = Number(fontSize) * ratio;
-      // 参考：https://developer.mozilla.org/zh-CN/docs/Web/CSS/font
-      // marHeight在这里是行高
-      ctx.font = `normal normal ${fontWeight} ${markSize}px/${markHeight}px undefined`;
-      ctx.textAlign = 'start';
-      ctx.textBaseline = 'top';
-      ctx.fillStyle = fontColor;
-      ctx.fillText(content, 0, 0);
-
-      cavasRef.current && setBase64Url(cavasRef.current.toDataURL());
-    }
-  }, [ctx, width, height, cavasRef, alpha, setBase64Url]);
+    watermarkRef.current && observer.observe(watermarkRef.current, config);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   return (
     <div
       className={className}
-      style={{ position: 'relative', overflow: 'hidden', ...style }}
+      style={{ position: "relative", overflow: "hidden", ...style }}
+      ref={watermarkRef}
     >
       <div
         style={{
-          position: 'absolute',
+          position: "absolute",
           top: 0,
           left: 0,
           right: 0,
           bottom: 0,
+          backgroundSize: `${gapX + width}px`,
           backgroundImage: `url(${base64Url})`,
         }}
       ></div>
